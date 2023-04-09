f68fda45f80eecf386b19b1ea27ad909d5634b5b
feb199 <feb199@outlook.com>
2023-04-09 23:54:09 +0100

    Added example tests to test.js

--- test.js
+++ test.js
@@ -1,7 +1,128 @@
+"use strict";
+
+//#region Setting up ResultBase
+const { Result, Results, ResultBase } = require("./classes/result.js");
+const logLevelsEnum = ResultBase.logLevelsEnum;
+ResultBase.currentLogLevel = logLevelsEnum.INFO;
+//#endregion
+
 
 // if(esMain(import.meta)) { // Checking if module is ran by itself (.mjs)
 if (require.main === module) { // Checking if module is ran by itself (.js)
     // ResultJS();
+
+
+    console.log("Test1+2");
+    //                     Name(String)   LogLevelType(EnumItem)     HTTPStatusCode(Number)   action(Number)         Message(String)          value(Any)
+    let result = new Result(  "Test"  ,     logLevelsEnum.INFO     ,           200           ,       1       , "Test completed successfully", [0, 1, 7, 3]);
+
+    //                       Name(String)   LogLevelType(EnumItem)     HTTPStatusCode(Number)   action(Number)         Message(String)
+    let result2 = new Result(  "Test2"  ,     logLevelsEnum.INFO     ,           400           ,       0       , "Test2 failed: user error");
+
+    result.setChild(result2);
+    console.log("\n\nresult.print();");
+    result.print();
+
+    console.log("\n\n\n\nresult.printMore();");
+    result.printMore();
+
+
+    console.log("\n\n\n\n\nTest3+4");
+
+
+    let result4 = new Result("Test4", logLevelsEnum.INFO, 200, 1, "Test4 completed successfully", [0, 1, 7, 3]);
+
+    result4.setParent(new Result("Test3", logLevelsEnum.INFO, 400, 0, "Test3 failed: user error"));
+
+    console.log("\n\nresult4.print();");
+    result4.print();
+
+    console.log("\n\n\n\nresult4.printMore();");
+    result4.printMore();
+
+    console.log("\n\n\n\nresult4.printMore(2);");
+    result4.printMore(2);
+
+
+    console.log("\n\n\n\n\nTest5+6+7+8");
+
+
+    let result5 = new Result("Test5", logLevelsEnum.INFO, 204, 1, "Test5 completed successfully")
+
+    result5.setChild(new Results("Test6", 200, 1, [
+        new Result("Test7", logLevelsEnum.INFO, 400, 0, "Test7 failed: user error"),
+        new Result("Test8", logLevelsEnum.INFO, 200, 1, "Test7 completed successfully", "Returned Text")
+    ]));
+
+    console.log("\n\nresult5.print();");
+    result5.print();
+
+    console.log("\n\n\n\nresult5.printMore();");
+    result5.printMore();
+
+
+    console.log("\n\n\n\n\nTest9+10+testFunct*3");
+
+
+    function testFunct(testVar = null) {
+        if(testVar === false) return new Result("Test10: testFunct", logLevelsEnum.WARN, 503, 0, "testVar is false");
+        if(testVar === null) return new Result("Test10: testFunct", logLevelsEnum.WARN, 500, 0, "testVar is set to an incorrect value");
+        return new Result("Test10: testFunct", logLevelsEnum.WARN, 200, 1, "testVar is set to a correct value");
+    }
+
+    let result9 = new Result("Test9", logLevelsEnum.INFO, 204, 1, "Test5 completed successfully")
+
+    let testFunctResults = [
+        testFunct(),
+        testFunct(false)
+    ]
+    testFunctResults.push(testFunct(true));
+    result9.setChild(new Results("Test10", 204, 1, testFunctResults));
+
+
+    console.log("\n\nresult9.print();");
+    result9.print();
+
+    console.log("\n\n\n\nresult9.printMore();");
+    result9.printMore();
+
+
+
+
+    console.log("\n\n\n\n\nPractical Example:");
+
+    var numOfDice = 2;
+    var total = null;
+
+    function setTotalVar(inputTotal) {
+        inputTotal = Number(inputTotal);
+        if(isNaN(inputTotal)) return new Result("Main: setTotal", logLevelsEnum.INFO, 400, 0, "Need a number");
+        if(inputTotal < numOfDice || inputTotal > numOfDice * 6) return new Result("Main: setTotal", logLevelsEnum.INFO, 400, 0, "Cannot set less than numOfDice or set higher than possible (numOfDice * 6)");
+        total = inputTotal;
+        return new Result("Main: setTotal", logLevelsEnum.DEBUG, 204, 1, `Set 'total' to ${inputTotal}`);
+    }
+
+    function totalVarCallback(totalInput) {
+        let result;
+        result = setTotalVar(totalInput);
+
+        if(result.action) {
+            return result.setParent(new Result("Main - total - totalCallback", logLevelsEnum.DEBUG, 204, 1, "Main totalCallback success"));
+        } else {
+            return result.setParent(new Result("Main - total - totalCallback", logLevelsEnum.INFO, 400, 0, "Main totalCallback failed - believed to be client error"));
+        }
+    }
+
+    let resultMain = new Result("Main", logLevelsEnum.INFO, 204, 1, "Main executed successfully");
+    resultMain.setChild(totalVarCallback(5));
+
+    ResultBase.currentLogLevel = logLevelsEnum.INFO;
+    console.log("\n\n\nResultBase.currentLogLevel = logLevelsEnum.INFO;\nresultMain.printMore();");
+    resultMain.printMore();
+
+    ResultBase.currentLogLevel = logLevelsEnum.TRACE;
+    console.log("\n\n\nResultBase.currentLogLevel = logLevelsEnum.TRACE;\nresultMain.printMore();");
+    resultMain.printMore();
 }
 
 function ResultJS() {
